9f890daf8fa7869b14dc6cde102bb65a8a4acf7a
Tomas Perez <tomasitopg6@gmail.com>
2020-10-01 16:20:49 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,11 +17,17 @@ class PostsController < ApplicationController
   	redirect_to post_path(@post)
 	end
 
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
 	def update
   	@post = Post.find(params[:id])
   	@post.update(post_params(:title))
   	redirect_to post_path(@post)
 	end
+
+	
 
 	private
 
